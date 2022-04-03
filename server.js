cf5b71d9f4b76c2861a5181b9dc5d1e04b69ab92
Nick <nickatodd@gmail.com>
2022-04-03 06:29:58 -0700

    Add websockets code

--- server.js
+++ server.js
@@ -1,17 +1,54 @@
+// FOR DIGITAL OCEAN
+
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const { createServer } = require('http');
+
+const WebSocket = require('ws');
+
+const app = express();
+app.use(express.static(path.join(__dirname, '/public')));
+
+const server = createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function (ws) {
+  const id = setInterval(function () {
+    ws.send(JSON.stringify(process.memoryUsage()), function () {
+      //
+      // Ignore errors.
+      //
+    });
+  }, 100);
+  console.log('started client interval');
+
+  ws.on('close', function () {
+    console.log('stopping client interval');
+    clearInterval(id);
+  });
+});
+
+server.listen(8080, function () {
+  console.log('Listening on http://0.0.0.0:8080');
+});
+
+// FOR HEROKU
+/*
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 
 const app = express()
 
-//here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-// this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function (req, res) {
+function (req, res) {
 	res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
 const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`app is listening on port: ${port}`)
+*/
