f128413b318767ca054b71abb3f776f309dafc3b
Nick <nickatodd@gmail.com>
2022-04-04 08:04:09 -0700

    Remove ws code

--- server.js
+++ server.js
@@ -12,23 +12,23 @@ const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
 
 const server = createServer(app);
-const wss = new WebSocket.Server({ server });
+// const wss = new WebSocket.Server({ server });
 
-wss.on('connection', function (ws) {
-  const id = setInterval(function () {
-    ws.send(JSON.stringify(process.memoryUsage()), function () {
-      //
-      // Ignore errors.
-      //
-    });
-  }, 100);
-  console.log('started client interval');
+// wss.on('connection', function (ws) {
+//   const id = setInterval(function () {
+//     ws.send(JSON.stringify(process.memoryUsage()), function () {
+//       //
+//       // Ignore errors.
+//       //
+//     });
+//   }, 100);
+//   console.log('started client interval');
 
-  ws.on('close', function () {
-    console.log('stopping client interval');
-    clearInterval(id);
-  });
-});
+//   ws.on('close', function () {
+//     console.log('stopping client interval');
+//     clearInterval(id);
+//   });
+// });
 
 server.listen(8080, function () {
   console.log('Listening on http://0.0.0.0:8080');
@@ -45,7 +45,7 @@ const app = express()
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 function (req, res) {
-	res.sendFile(path.join(__dirname, '/dist/index.html'))
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
 const port = process.env.PORT || 8080
